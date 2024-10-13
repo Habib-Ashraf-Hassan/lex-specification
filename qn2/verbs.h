f928feb08141fe8a1e80447180c7bc421f5a384f
Ashraf_Mohammed_Anil <100794988+Habib-Ashraf-Hassan@users.noreply.github.com>
2024-10-13 18:03:33 +0300

    Add files via upload

---
+++ qn2/verbs.h
@@ -0,0 +1,12 @@
+/* verbs.h */
+#ifndef VERBS_H
+#define VERBS_H
+
+/* List of verbs */
+char *verbs[] = {
+    "is", "am", "are", "were", "was", "be", "being", "been",
+    "do", "does", "did", "will", "would", "should", "can", "could",
+    "has", "have", "had", "go", NULL
+};
+
+#endif /* VERBS_H */
